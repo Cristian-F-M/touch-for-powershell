d1a4e28ab6162a3c81482fa72b7c357d8fdc9f09
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-14 16:25:38 -0500

    Add New-File function

---
+++ Functions/New-File.ps1
@@ -0,0 +1,25 @@
+function New-File {
+  param ([string]$path)
+
+  $path = Clear-Spaces $path
+  if (Is-Empty $path) {
+    Show-MessageWithColor "Path is empty" -Color Red
+    return $null
+  }
+  
+  if (Test-Path $path) {
+    Show-MessageWithColor "File already exists" -color Red
+    return $null
+  }
+  
+  New-Item -Path $path -ItemType File -Force | Out-Null
+
+  if (-not (Test-File $path)) {
+    Show-MessageWithColor "File not created" -color Red
+    return $null
+  }
+
+  Show-MessageWithColor "File created" -Color Green
+  return $null
+
+}
